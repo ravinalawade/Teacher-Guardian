c10694bcaa16cdae3a53c220029f2455adc9d378
Pratik Naik <pratiknaik4799@gmail.com>
2019-09-10 19:19:12 +0530

    Added basic title

---
+++ admin_panel.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <title>Login Form</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Admin Panel</h1>
+        </div>
+    </body>
+</html>
